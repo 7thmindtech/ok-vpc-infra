74fda3677ce692486e17f67a6f4b9e6b0c026b63
chimezdev <ochuba6@gmail.com>
2025-05-19 14:05:10 -0400

    create private link

--- route-tables.tf
+++ route-tables.tf
@@ -31,14 +31,15 @@ resource "aws_route" "pub_route" {
 resource "aws_route_table" "priv_route_tab" {
   vpc_id = aws_vpc.main.id
 
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_vpc_endpoint.s3.id
-  }
-
-  depends_on = [aws_vpc_endpoint.s3]
 
   tags = merge(local.tags, tomap({ "Name" = "${var.cust_name}-private-route-table" }))
+}
+resource "aws_route" "priv_route" {
+  route_table_id         = aws_route_table.priv_route_tab.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_vpc_endpoint.s3.id
+
+  depends_on = [aws_vpc_endpoint.s3]
 }
 
 resource "aws_route_table_association" "pub_sub_assoc" {
